ac71a6df5dfda582cc9eea36acedb6bdd9700f1e
baraamukayed <53788298+baraamukayed@users.noreply.github.com>
2019-12-20 17:09:32 +0300

    edit

--- app/src/main/java/com/example/androidproject/signUpActivity.java
+++ app/src/main/java/com/example/androidproject/signUpActivity.java
@@ -7,16 +7,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class signUpActivity extends AppCompatActivity {
 
     TextView exitTV,signInTV;
+    FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user!=null)
+        {
+            Intent intent = new Intent(signUpActivity.this , MainActivity.class);
+            startActivity(intent);
+        }
+
     }
 
     public void onClickSI(View v) {
