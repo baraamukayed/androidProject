cca8d1a5667cc05edc1f8dd6fda9265e16ffa26c
abdlulu <abdlulu1@gmail.com>
2019-12-21 16:45:14 +0200

    added note rv

---
+++ app/src/main/java/com/example/androidproject/Note.java
@@ -0,0 +1,60 @@
+package com.example.androidproject;
+
+public class Note {
+    String id;
+    String notebookID;
+    String desc;
+    long createdAt;
+    long lastUpdate;
+
+
+    public Note(){}
+
+    public Note(String id,String notebookID, String desc, long createdAt, long lastUpdate) {
+        this.id = id;
+        this.notebookID = notebookID;
+        this.desc = desc;
+        this.createdAt = createdAt;
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNotebookID() {
+        return notebookID;
+    }
+
+    public void setNotebookID(String notebookID) {
+        this.notebookID = notebookID;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public long getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(long lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+}
